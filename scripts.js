600624fffe0597d2594fd077512919c6040002c2
Tor <tor.devries@wsu.edu>
2022-10-21 18:53:17 +0000

    added palette interactivity, starting avatar preview

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
 /* code for Alien Factory */
+
+function changePalette(toFeature) {
+	document.querySelector("#palette").classList.remove("showBody");
+	document.querySelector("#palette").classList.remove("showFace");
+	document.querySelector("#palette").classList.remove("showArms");
+	document.querySelector("#palette").classList.remove("showFeet");
+	document.querySelector("#palette").classList.add(toFeature);
+}
+
+
+
+window.onload = function() {
+	
+	/* onclick functions for palette shifting */
+	document.querySelector("#featuresBody h1").onclick = function() {
+		changePalette("showBody");
+	}
+	document.querySelector("#featuresFace h1").onclick = function() {
+		changePalette("showFace");
+	}
+	document.querySelector("#featuresArms h1").onclick = function() {
+		changePalette("showArms");
+	}
+	document.querySelector("#featuresFeet h1").onclick = function() {
+		changePalette("showFeet");
+	}
+	
+	/* onclick functions for avatar preview updates */
+	document.querySelector("#bodyOption1").onclick = function() {
+		
+		document.querySelector("#previewBody").classList.add("robot");
+		
+	}
+	
+}
